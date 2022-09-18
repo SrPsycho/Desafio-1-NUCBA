d9bf50dcc3675cd60a4b72e890e9a8b18b3cc4d7
Matias Hausen <matihausen@gmail.com>
2022-09-18 12:54:38 -0300

    Creando el CSS

---
+++ script.js
@@ -0,0 +1,11 @@
+var cerrar=false;
+
+
+function sesion(){
+    document.getElementById("cerrarSesion");
+    
+}
+
+function cerrarVentana(){
+    document.getElementById("overlay").style.display="none";
+}
